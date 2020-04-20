fdd80ff1d29add42d93262957ffd052a0ff187e9
RenuJaishankar <Renu.Jaishankar@yahoo.com>
2020-04-20 09:55:03 -0400

    heroku uri added

--- src/Apollo/client.js
+++ src/Apollo/client.js
@@ -2,6 +2,7 @@ import ApolloClient from 'apollo-boost'
 import fetch from 'isomorphic-fetch'
 
 export const client = new ApolloClient({
-uri: 'http://localhost:8080/graphql',
+// uri: 'http://localhost:8080/graphql',
+uri:'https://intense-brook-67705',
 fetch,
 })
